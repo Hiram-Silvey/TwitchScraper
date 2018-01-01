5e0ff24f5be35d0ea78387d5cd64bfbc30ba46f1
Hiram Silvey <blujay270@gmail.com>
2018-01-01 17:40:09 -0500

    normalized time data and made initial range alg

--- VOD/data.py
+++ VOD/data.py
@@ -1,26 +1,39 @@
+"""Usage:
+    data.py <csvfile>
+"""
+
 import requests
 import time
 import calendar
+from docopt import docopt
 import cfg
+from IPython import embed
 
-def expand_time(hms_time):
-    h_idx = hms_time.find('h')
-    m_idx = hms_time.find('m')
-    s_idx = hms_time.find('s')
-    hours = int(hms_time[:h_idx]) if h_idx != -1 else 0
-    mins = int(hms_time[h_idx+1:m_idx]) if m_idx != -1 else 0
-    secs = int(hms_time[m_idx+1:s_idx]) if s_idx != -1 else 0
+args = docopt(__doc__)
+
+def expand_date_time(date_time):
+    date_time = date_time[date_time.find('T')+1:-1]
+    hours, mins, secs = map(int, date_time.split(':'))
+    return ((hours*60)+mins)*60+secs
+
+def expand_duration(duration):
+    h_idx = duration.find('h')
+    m_idx = duration.find('m')
+    s_idx = duration.find('s')
+    hours = int(duration[:h_idx]) if h_idx != -1 else 0
+    mins = int(duration[h_idx+1:m_idx]) if m_idx != -1 else 0
+    secs = int(duration[m_idx+1:s_idx]) if s_idx != -1 else 0
     return ((hours*60)+mins)*60+secs
 
 r = requests.get('https://api.twitch.tv/helix/videos?user_id={}'.format(cfg.USER_ID), headers={'Client-ID': cfg.CLIENT_ID, 'Accept': 'application/vnd.twitchtv.v5+json'})
 
 vods = r.json()['data']
 pattern = '%Y-%m-%dT%H:%M:%SZ'
-with open('metadata.csv', 'w+') as f:
+with open(args['<csvfile>'], 'w+') as f:
     for vod in vods:
         date_time = vod['created_at']
         duration = vod['duration']
-        end_time = calendar.timegm(time.strptime(date_time, pattern))
-        start_time = end_time - expand_time(duration)
+        end_time = expand_date_time(date_time)
+        start_time = end_time - expand_duration(duration)
         vod_id = vod['id']
         f.write('{}\t{}\t{}\n'.format(vod_id, start_time, end_time))
--- VOD/range.py
+++ VOD/range.py
@@ -1,42 +1,42 @@
 """Usage:
-    range.py <mdfile>
+    range.py <incsv> <outcsv> <overlap_in_minutes>
 """
 
 from docopt import docopt
 
 args = docopt(__doc__)
 
+min_overlap = int(args['<overlap_in_minutes>'])*60
+
 starts = []
 ends = []
 start_to_end = {}
 end_to_start = {}
 
-with open(args['<mdfile>'], 'r') as f:
+with open(args['<incsv>'], 'r') as f:
     for line in f:
         vid, start, end = map(int, line.split('\t'))
         starts.append(start)
-        start_to_end[start] = end
+        start_to_end[start] = (vid, end)
         ends.append(end)
         end_to_start[end] = start
 
 starts.sort()
 ends.sort(reverse=True)
 
-while len(starts) > 0 and starts[-1] > ends[-1]:
-    print(starts)
-    print(ends)
-    start_gain = ends[-1] - starts[-2]
-    end_gain = ends[-2] - starts[-1]
-    print(start_gain)
-    print(end_gain)
+while len(starts) > 0 and starts[-1] >= ends[-1]+min_overlap:
+    next_end = ends[-1] if start_to_end[starts[-1]][1] != ends[-1] else ends[-2]
+    start_gain = next_end - starts[-2]
+    next_start = starts[-1] if end_to_start[ends[-1]] != starts[-1] else starts[-2]
+    end_gain = ends[-2] - next_start
     if start_gain > end_gain:
         start = starts.pop()
-        end = start_to_end[start]
-        ends.remove(end)
+        ends.remove(next_end)
     else:
         end = ends.pop()
-        start = end_to_start[end]
-        starts.remove(start)
+        starts.remove(next_start)
 
-print(starts)
-print(ends)
+with open(args['<outcsv>'], 'w+') as f:
+    f.write('{}\t{}\n'.format(starts[-1], ends[-1]))
+    for start in starts:
+        f.write('{}\n'.format(start_to_end[start][0]))
