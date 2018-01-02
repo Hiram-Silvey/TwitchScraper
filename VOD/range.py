cc84fd2b506bb7ada0e88f12ad5100470c674e46
Hiram Silvey <blujay270@gmail.com>
2018-01-02 15:23:26 -0500

    fixed bug where range was not removing corresponding start and end times properly

--- VOD/range.py
+++ VOD/range.py
@@ -3,6 +3,7 @@
 """
 
 from docopt import docopt
+from IPython import embed
 
 args = docopt(__doc__)
 
@@ -24,19 +25,21 @@ with open(args['<incsv>'], 'r') as f:
 starts.sort()
 ends.sort(reverse=True)
 
-while len(starts) > 0 and (starts[-1]+min_overlap)%86400 >= ends[-1]:
-    next_end = ends[-1] if start_to_end[starts[-1]][1] != ends[-1] else ends[-2]
+while len(starts) > 1 and (starts[-1]+min_overlap)%86400 >= ends[-1]:
+    linked_end = start_to_end[starts[-1]][1]
+    next_end = ends[-1] if linked_end != ends[-1] else ends[-2]
     start_gain = next_end - starts[-2]
-    next_start = starts[-1] if end_to_start[ends[-1]] != starts[-1] else starts[-2]
+    linked_start = end_to_start[ends[-1]]
+    next_start = starts[-1] if linked_start != starts[-1] else starts[-2]
     end_gain = ends[-2] - next_start
     if start_gain > end_gain:
         start = starts.pop()
-        ends.remove(next_end)
+        ends.remove(linked_end)
     else:
         end = ends.pop()
-        starts.remove(next_start)
+        starts.remove(linked_start)
 
 with open(args['<outcsv>'], 'w+') as f:
     f.write('{}\t{}\n'.format(starts[-1], ends[-1]))
     for start in starts:
-        f.write('{}\n'.format(start_to_end[start][0]))
+        f.write('{}\t{}\t{}\n'.format(start_to_end[start][0], start, start_to_end[start][1]))
