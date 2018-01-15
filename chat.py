2d43f5c603d3c8930ae9d935aa56d315a21c2f25
Asciotti53 <andrew.sciotti@gmail.com>
2018-01-14 22:21:21 -0500

    Finds first instance of time, gets comments until end time, doesnt iterate over multiple vid-ids yet

--- chat.py
+++ chat.py
@@ -4,32 +4,62 @@
  
 
 import requests
-import numpy as np
 chat = []
- 
 
-def get_comments(vid_id, video_start, video_end, desire_start,desire_end):
 
-    r = requests.get('https://api.twitch.tv/v5/videos/'+str(vid_id)+'/comments?client_id=9jkelimecr9yevu0ez4n51orefunjv')
 
-    data = r.json()['comments']
-    for x in data:
-        body = str(test['message']['body'])
-        name = str(test['commenter']['display_name'])
-        time = str(test['content_offset_seconds'])
-        line = [name, time, body]
-        if line not in chat:
-            chat.append(line)
+def get_comments(vid_id, vstart, vend):
+    start = 39483
+    end = 43778
+    duration = end-start if end-start >= 0 else 86400-start+end
+    offset_start = start-vstart if start-vstart >= 0 else 86400-start+vstart
+    url = 'https://api.twitch.tv/v5/videos/'+str(vid_id)+'/comments?client_id=9jkelimecr9yevu0ez4n51orefunjv'
+    #try to find first chunk of data with desired start time
+    found_data = 0 #flag to say if we found the chunk of 59 with the time we want
+    entry_data = 0 #so we can keep track of which entry in a chunk of 59 is the start of the comments we want
+    cursor = 0
+    while !found_data:
+        if cursor!=0:
+            url = url+'&cursor='+str(cursor)
+        r = requests.get(url)
+        current_cursor = cursor
+        cursor = r.json()['_next']
+        data = r.json()['comments']
+        for i,x in enumerate(data):
+            if(x['content_offset_seconds']>=offset_start):
+                found_data = 1 #set flag to found
+                entry_data = i
+                cursor=current_cursor #so that we start from the chunk of comments that we found the correct time in
+                end_time = x['content_offset_seconds'] + duration #for later when we need to stop finding comments
+                break
+    #now that we have found the data and the entry number we need to go until we get to the end
+    stop_searching = 0 #flag for when to stop getting comments
+    first_data = 1 #flag for first data set we pull only needs to start at entry_data
+    while !stop_searching:
+        url = url+'&cursor='+str(cursor) 
+        r = requests.get(url)
+        cursor = r.json()['_next']
+        data = r.json()['comments']      
+        for i,x in enumerate(data):
+            body = str(x['message']['body'])
+            name = str(x['commenter']['display_name'])
+            time = str(x['content_offset_seconds'])
+            line = [name, time, body]
+            if first_data:
+                if i>=entry_data and line not in chat and time<=end_time:
+                    chat.append(line)
+                    first_data=0
+            else if line not in chat and time<=end_time:
+                chat.append(line)
+            else if time>end_time:
+                stop_searching=1
+                break
+
    
 
     
 
- 
-
-#get_comments(1,1)
-
-
-##I can give you video IDs, video start times, video end times, and the specific start and end time that we want
+ ##I can give you video IDs, video start times, video end times, and the specific start and end time that we want
 ##times are in the format: ((hour*60)+mins)*60+secs
 ##seconds from 00:00 in the day
 ##so 16:43:23 = ((16*60)+43)*60+23 = 60203
